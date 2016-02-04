76b83e2268c612b83c9b90323d33cfa8e9268284
interferon <myvictoryaremko@gmail.com>
2016-02-04 16:02:22 +0200

    commit

--- bundle.js
+++ bundle.js
@@ -47,7 +47,7 @@
 	var s = document.createElement('script');
 	s.src = chrome.extension.getURL('injected.js');
 	s.onload = function() {
-	    this.parentNode.removeChild(this);
+	    // this.parentNode.removeChild(this);
 	};
 	(document.head || document.documentElement).appendChild(s);
 
--- injected.js
+++ injected.js
@@ -1,68 +1,66 @@
+var xhr = window.XMLHttpRequest;
+
 var storage = [];
 
-function saveCache(data){
-	console.log('stored');
-	storage.push(data);
-	console.log(storage.length);
-}
-
-function extractCached(data){
-	console.log("cache extracted");
-	return storage.filter(function(current, index, arr){
-		return JSON.stringify(current.url) === JSON.stringify(data.url) && JSON.stringify(current.post) === JSON.stringify(data.post); 
-	})[0].loadedXHR;
-}
-
-function isCached(data){
-	console.log('checking for cached');
-	var cached = storage.filter(function(current, index, arr){
-		return JSON.stringify(current) === JSON.stringify(data); 
-	});
-	return cached.length > 0;
-}
-
-function checkCache(data, abortReq, assignCached, callOriginalSend){
-	console.log(isCached(data));
-	if (!isCached(data)){
-		saveCache(data);
-		callOriginalSend();
-	}
-	else{
-		console.log("req abort");
-		abortReq();
-		assignCached(extractCached(data));
-	}
-}
-
-var orig_open = XMLHttpRequest.prototype.open;
-XMLHttpRequest.prototype.open = function(method, url, async, user, pass){
-	this.__url = url;
-	orig_open.call(this, method, url, async, user, pass);
-};
-
-var orig_send = XMLHttpRequest.prototype.send;
-XMLHttpRequest.prototype.send = function(post_data){
-	var that = this;
-	function abortReq(){
-		that.abort();
-	}
-	function callOriginalSend(){
-		console.log("called");
-		orig_send.call(that, post_data);
-	}
-	this.addEventListener('loadend', function(){
-		console.log('loaded');
-		checkCache({
-			url : that.__url,
-			post : post_data,
-			loadedXHR : that
-		},
-		abortReq,
-		function(cached){
-			that = cached;
-		},
-		callOriginalSend
-		);
-	});
+window.XMLHttpRequest = function (){
 	
+	var myXHR = new xhr();
+
+	function saveCache(data){
+		console.log('stored');
+		storage.push(data);
+	}
+
+	function getCached(data){
+		console.log("cache extracted");
+		return storage.filter(function(current, index, arr){
+			return JSON.stringify(current.url) === JSON.stringify(data.url) && JSON.stringify(current.post) === JSON.stringify(data.post); 
+		})[0].loadedRes;
+	}
+
+	function toBeCopied(attr){
+		var toBeReplaced = ['open', 'send', 'readyState'];
+		return !toBeReplaced.includes(attr);
+	}
+
+	for (attr in xhr){
+		if (toBeCopied(attr)){
+			myXHR.attr = xhr[attr]; 
+		}
+	}
+
+	myXHR.open = function(method, url, async, user, pass){
+		console.log('opened');
+		this.__url = url;
+		orig_open.call(this, method, url, async, user, pass);
+	};
+	myXHR.send = function(post_data){
+		var reqData = {url : this.__url,post : post_data};
+		this.addEventListener('readystatechange', function(){
+			if (this.readyState == 4) {
+				if(this.status == 200) {
+					reqData.loadedRes = this.responseText;
+					saveCache(reqData);
+				}
+			}
+			else{
+				return;
+			}
+		});
+		if (!cached(reqData)){
+			console.log('req sent');
+			orig_send.call(this, post_data);
+		}else{
+			this.responseText = getCached(reqData);
+			this.readyState = 4;
+			this.status = 200;
+			this.onreadystatechange();
+		}
+
+	}
+	
+	myXHR.readyState = 0;
+	
+	return myXHR;
+
 };
