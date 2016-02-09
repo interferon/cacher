75f135f99a7cdf9f2935bd9177c63e71911f09f9
interferon <myvictoryaremko@gmail.com>
2016-02-09 13:53:26 +0200

    user warn added to popup

--- src/popup.js
+++ src/popup.js
@@ -2,23 +2,33 @@ document.getElementById('submit_fn').addEventListener(
 	"click",
 	function(){
 		var fnbody = document.getElementById('custom_id_fn').value.trim();
-		chrome.tabs.query(
-			{
-				active: true,
-				currentWindow: true
-			},
-			function(tabs) {
-		  		chrome.tabs.sendMessage(
-		  			tabs[0].id,
-		  			{
-		  				reciever : "identityFnBody",
-		  				data : fnbody
-		  			},
-		  			function(response) {
-		    			console.log(response.farewell);
-		  			}
-		  		);
-			}
-		);
+		notifyContentScript(fnbody);
+		showUserWarn('Changes will take effect after page refresh');
 	}
 );
+
+function notifyContentScript(fnbody){
+	chrome.tabs.query(
+		{
+			active: true,
+			currentWindow: true
+		},
+		function(tabs) {
+	  		chrome.tabs.sendMessage(
+	  			tabs[0].id,
+	  			{
+	  				reciever : "identityFnBody",
+	  				data : fnbody
+	  			},
+	  			function(response) {
+	    			console.log(response.farewell);
+	  			}
+	  		);
+		}
+	);
+}
+
+function showUserWarn (message) {
+	var userMessageElem = document.getElementById("user_message")
+	userMessageElem.innerHTML = message;
+}
